c6e6f48a3393915dfea26e55b10530ae5cc81652
Sumit <sj.sumitjangir@gmail.com>
2022-12-30 01:06:51 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sjsumitjangir007/fyne_route
+
+go 1.19
+
+require fyne.io/fyne/v2 v2.3.0
---
+++ route.go
@@ -0,0 +1,37 @@
+package route
+
+import "fyne.io/fyne/v2"
+
+func init() {
+	// Initialization of route
+}
+
+// Page structure
+type Page struct {
+	container *fyne.Container
+}
+
+// Route
+type Route struct {
+	container *fyne.Container
+	pages     map[string]Page
+}
+
+func (r *Route) Start() *fyne.Container {
+	for key, value := range r.pages {
+		if key == "/" {
+			r.container.Add(value.container)
+		}
+	}
+	return r.container
+}
+
+func (r *Route) Load(path string) {
+	r.container.RemoveAll()
+	for key, value := range r.pages {
+		if key == path {
+			r.container.Add(value.container)
+		}
+	}
+	r.container.Refresh()
+}
